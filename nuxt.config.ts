0eab6f1585a908acd27623252e50546e525e1725
wai-lin <dev.wailin@gmail.com>
2025-12-02 12:16:56 +0700

    feat: update icons and project images

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,7 @@ export default defineNuxtConfig({
 	compatibilityDate: '2025-07-15',
 	devtools: { enabled: true },
 	css: ['~/assets/css/main.css'],
+	modules: ["@nuxt/icon"],
 	vite: {
 		plugins: [tailwindcss()]
 	},
