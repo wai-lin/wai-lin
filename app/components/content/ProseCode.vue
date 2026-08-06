1a8c333a8a555eb425e79405bed6c19b0b5fbba0
wai-lin <dev.wailin@gmail.com>
2026-08-06 08:55:46 +0700

    style: use emerald color for code block

--- app/components/content/ProseCode.vue
+++ app/components/content/ProseCode.vue
@@ -1,5 +1,5 @@
 <template>
-	<code class="bg-accent/80 text-accent-foreground rounded px-1.5 py-0.5 font-mono text-[0.875em]">
+	<code class="rounded bg-emerald-200/50 px-1.5 py-0.5 font-mono text-[0.875em] text-emerald-900">
 		<slot />
 	</code>
 </template>
